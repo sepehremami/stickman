fd2df751a7beed8d5b4420effe958993a1a23268
sepehr emami <sepehr7890@gmail.com>
2023-10-03 02:38:29 +0330

    delete player, enemy

--- core/player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self) -> None:
-        self.money = 1000
-
-    def get_money(self):
-        return self.money
-
-    def spend_money(self, money):
-        self.money -= money
